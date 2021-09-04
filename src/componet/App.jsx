3c80dc393b30a8411747d183f6c37783f129a7df
anujmittal2308 <84795217+anujmittal2308@users.noreply.github.com>
2021-09-04 16:43:47 +0530

    Initial commit

---
+++ src/componet/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "/src/componet/Heading";
+import List from "/src/componet/List";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+      <Heading />
+    </div>
+  );
+}
+export default App;
